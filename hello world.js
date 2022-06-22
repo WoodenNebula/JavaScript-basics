2ed7378c9deecd956801edf97f1855412af65e55
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-06-22 13:43:02 +0545

    Log console

--- hello world.js
+++ hello world.js
@@ -1,4 +1,14 @@
-function log(){
+function helloWorld(){
     return "Hello World"
 }
-log()
+console.log(helloWorld())
+console.log(helloWorld)
+
+const showName = () =>{
+    let name = "Surab"
+    let surname = "Parajuli"
+    console.log(name + " " + surname)
+}
+
+console.log("This is " + showName)
+console.log("And this is " + showName())
